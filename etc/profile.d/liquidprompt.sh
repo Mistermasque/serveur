26f8ed9ba07a7cfbe5f484b0b194f7249581bce4
Mistermasque <mistermasque@gmail.com>
2022-12-29 15:13:22 +0100

    Correction chargement liquidprompt si non installé

--- etc/profile.d/liquidprompt.sh
+++ etc/profile.d/liquidprompt.sh
@@ -1,2 +1,11 @@
-source /opt/liquidprompt/liquidprompt
+# Il faut avoir installé liquidprompt avec le paquet debian pour que cela fonctionne
+# apt install liquidprompt
+if [ -f "/usr/share/liquidprompt/liquidprompt" ]; then
+    source /usr/share/liquidprompt/liquidprompt
+fi
+
+# Sinon installer manuellement liquidprompt dans /opt/liquidprompt/
+if [ -f "/opt/liquidprompt/liquidprompt" ]; then
+    source /opt/liquidprompt/liquidprompt
+fi
 
